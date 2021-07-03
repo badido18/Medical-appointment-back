73696bf40b32b664b1d9ea804797474753338d0a
Doumi Athmane <ha_doumi@esi.dz>
2021-07-03 23:15:14 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,8 +33,8 @@ const getPatients = (req, res) => {
 const getMedecins = (req, res) => {
     Medecin.find()
     .then(users => {
-        //res.send(success("list of medecins : ", users))
-        res.send(success(users))
+        res.send(success("list of medecins : ", users))
+        
     })
     .catch(err => {
         res.send(error(err.message));
