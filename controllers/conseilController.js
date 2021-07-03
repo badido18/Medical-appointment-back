7915752d1f18f41209a72dcfc06bb4a7805c006d
badido18 <badido18@live.fr>
2021-07-04 00:50:44 +0100

    feat : conseils

--- controllers/conseilController.js
+++ controllers/conseilController.js
@@ -1,8 +1,55 @@
+const { getRepository } = require("typeorm")
+const { error, success } = require("../lib/response")
 
-const addConseil = (req,res) => {}
-const deleteConseil= (req,res) => {}
-const getConseils = (req,res) => {}
-const getAllConseils = (req,res) => {}
+const Conseil = getRepository("conseil")
+
+const addConseil = (req,res) => {
+    const {traitmentId , type , description} = req.body
+    Conseil.save({traitmentId,type,description})
+    .then(hor => res.send(success("insertion avec success ", hor)))
+    .catch(err => {
+        res.send(error(err.message));
+    })
+}
+const deleteConseil= (req,res) => {
+    Conseil.delete({id:req.params.id})
+    .then( aff =>
+        res.send(success("suppression avec succes",aff))
+    )
+    .catch(err => {
+        res.send(error(err.message));
+    })
+}
+
+const getConseils = (req,res) => {
+    Conseil.find({traitmentId:req.params.id , type:"answer"})
+    .then(cons => {
+        res.send(success("liste de conseils : ", cons))
+    })
+    .catch(err => {
+        res.send(error(err.message));
+    })
+}
+
+const getDemandeConseils = (req,res) => {
+    Conseil.find({traitmentId:req.params.id , type:"request"})
+    .then(cons => {
+        res.send(success("liste des demande conseils : ", cons))
+    })
+    .catch(err => {
+        res.send(error(err.message));
+    })
+}
+
+const getAllConseils = (req,res) => {
+    Conseil.find()
+    .then(cons => {
+        res.send(success("liste de conseils : ", cons))
+    })
+    .catch(err => {
+        res.send(error(err.message));
+    })
+}
 
 
-module.exports =  { addConseil, deleteConseil, getConseils , getAllConseils }
+module.exports =  { addConseil, deleteConseil, getConseils , getAllConseils ,getDemandeConseils}
--- routes/conseil.js
+++ routes/conseil.js
@@ -1,11 +1,13 @@
 const { Router } = require("express")
-const { addConseil, deleteConseil, getConseils , getAllConseils } = require('../controllers/conseilController')
+const { addConseil, deleteConseil, getConseils , getAllConseils , getDemandeConseils } = require('../controllers/conseilController')
+const {adminCheck } = require("../lib/security")
 
 const router = Router()
 
 router.post('/add', addConseil)
-router.get('/:traitmentid', getConseils)
+router.get('/:id', getConseils)
+router.get('/reqs/:id', getDemandeConseils)
 router.delete('/:id', deleteConseil)
-router.get('/all', getAllConseils)
+router.get('/', adminCheck, getAllConseils)
 
 module.exports = router;
