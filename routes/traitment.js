71b0e31c3f3e4fe8611231482f4164b60b12f2a5
badido18 <badido18@live.fr>
2021-07-04 00:39:11 +0100

    feat : traitment

--- routes/traitment.js
+++ routes/traitment.js
@@ -1,11 +1,12 @@
 const { Router } = require("express")
 const { addTrait, deleteTrait, getTraits, getAllTraits } = require('../controllers/traitmentController')
+const {adminCheck } = require("../lib/security")
 
 const router = Router()
 
 router.post('/add', addTrait)
 router.delete('/:id', deleteTrait)
-router.get('/all', getAllTraits)
+router.get('/',adminCheck, getAllTraits)
 router.get('/:id', getTraits)
 
 module.exports = router;
